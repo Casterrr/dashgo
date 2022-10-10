123219b1e5bd0201a57dd04e49b6f4a856beedfc
Lucas <lucasmvl2003@gmail.com>
2022-10-10 12:55:31 -0300

    Starting with React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,40 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error } =  useQuery("users", async () => {
+        const response = await fetch("http://localhost:3000/api/users")
+        const data = await response.json()
+            
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                created_at: new Date(user.created_at).toLocaleDateString('pt-BT', {
+                    day: '2-digit',
+                    month:'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users
+    }, {
+        staleTime: 1000 * 5 // 5 seconds
+    })
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
      })
-  
 
     return(
         <Box>
@@ -36,38 +60,54 @@ export default function UserList() {
                        </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha" >
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8" >
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>
-                                    Usuário
-                                </Th>
-                                { isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="8" >
-                                    
-                                </Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Lucas Matheus</Text>
-                                        <Text fontSize="sm" color="gray.300" >lucmvl2003@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>05 de Outubro, 2022</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
+                    { isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha" >
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8" >
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>
+                                            Usuário
+                                        </Th>
+                                        { isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width="8" >
+                                            
+                                        </Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300" >{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && <Td>{user.created_at}</Td>}
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
+                            
+                            <Pagination />
+                        </>
+                    ) }
                 </Box>
             </Flex>
         </Box>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,79 @@
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs'
+import faker from '@withshepherd/faker'
+
+type User = {
+    name: string
+    email: string
+    created_at: string
+}
+
+export function makeServer() {
+    const server = createServer({
+
+        // Database tables
+        models: {
+            user: Model.extend<Partial<User>>({})
+        },
+
+        // Allows to save/get datas from 2 tables at once.
+        // serializers: {  
+        //     application: ActiveModelSerializer
+        // },
+
+        // Random data generation
+        factories: {  
+            user: Factory.extend({
+                name(i: number) {
+                    return faker.name.firstName() +' '+ faker.name.lastName()
+                    //return `Usuário ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase()
+                },
+                created_at() {
+                    return faker.date.recent(90) //Last 90 days
+                }
+            })
+        },
+
+        // Includin massive datas (10 users)
+        seeds(server) {  
+            server.createList('user', 10)
+        },
+
+        routes() {
+            this.namespace = 'api' 
+            this.timing = 750     // API call delay, for testing
+            
+            this.post('/users')   // route POST api/users              
+            this.get('/users')  // route GET api/users
+
+            // GET with pagination of 10 in 10
+            // this.get('/users', function (schema, request) {
+            //     const { page = 1, per_page = 10} = request.queryParams
+            //     const totalUsers = schema.all('user').length //returns 200
+            //     const pageStart = (Number(page) - 1) * Number(per_page)
+            //     const pageEnd = pageStart + Number(per_page)
+                
+            //     const users = this.serialize(schema.all('user'))
+            //         .users
+            //         //.sort((a, b) => b.name < a.name) não funciona
+            //         .slice(pageStart, pageEnd)
+
+            //     return new Response(
+            //         200, // status code
+            //         { 'x-total-count': String(totalUsers) }, //header
+            //         { users }
+            //     )
+            // })
+            
+            // this.get('/users/:id')
+            
+            this.namespace = ''   // if namespace isn't "api",this is not needed.
+            this.passthrough()    
+        }
+
+    })
+
+    return server
+}
